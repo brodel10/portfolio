e2b388a5845a21e8930fb02d4fe2634f5abffa47
brodel10 <rodeell21@gmail.com>
2025-03-01 10:20:52 -0800

    exception

--- components/video-section.tsx
+++ components/video-section.tsx
@@ -74,7 +74,7 @@ const VideoGrid: React.FC<VideoGridProps> = ({ videos, channelInfo }) => {
         className={`place-self-end md:col-span-2 transition-all duration-500 ease-out ${
           isVisible ? "translate-y-0 opacity-100" : "translate-y-10 opacity-0"
         }`}
-        style={{ transitionDelay: `${videos.length * 100}ms` }}
+        style={{ transitionDelay: `${videos?.length * 100}ms` }}
       >
         <Link
           href={"https://www.youtube.com/@brodelridesbikes"}
